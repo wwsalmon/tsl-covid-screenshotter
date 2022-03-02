8959bded89249e53b39b929c1ec3c13b57139aa1
Samson Zhang <wwsamson@yahoo.com>
2022-03-02 09:23:47 -0800

    clear broken scs and fix sc code

--- index.mjs
+++ index.mjs
@@ -1,9 +1,9 @@
 import captureWebsite from 'capture-website';
 
 const list = [
-    ["pomona", "https://tableau.campus.pomona.edu/views/DailyHealthCheckDashboard/PomonaCollegeCOVID-19FullWeeklyTestingData?%3AshowAppBanner=false&%3Adisplay_count=n&%3AshowVizHome=n&%3Aorigin=viz_share_link&%3Aembed=y", "#dashboard-viewport"],
-    ["scripps", "https://www.scrippscollege.edu/scripps-strong/covid-19-dashboard/", ".article__entry"],
-    ["cmc", "https://cmc-returns21.cmc.edu/covid-19-dashboard/", "#dashboard.et_pb_row_2"],
+    ["pomona", "https://tableau.campus.pomona.edu/views/DailyHealthCheckDashboard/PomonaCollegeCOVID-19FullWeeklyTestingData?%3AshowAppBanner=false&%3Adisplay_count=n&%3AshowVizHome=n&%3Aorigin=viz_share_link&%3Aembed=y", ".tab-vizHeader"],
+    ["scripps", "https://www.scrippscollege.edu/scripps-strong/covid-19-dashboard/", "canvas.tab-widget"],
+    ["cmc", "https://cmc-returns21.cmc.edu/covid-19-dashboard/", 5],
     ["hmc", "https://www.hmc.edu/covid-19/dashboard/"],
     ["pitzer", "https://www.pitzer.edu/pathway-forward/covid-19-dashboard/"],
 ];
@@ -12,6 +12,18 @@ const datePrefix = new Date().toLocaleDateString("sv");
 
 for (let item of list) {
     console.time(`screenshotting ${item[0]}`);
+
+    let options = {
+        fullPage: true,
+    };
+
+    if (item[2]) {
+        if (isNaN(item[2])) {
+            options["waitForElement"] = item[2];
+        } else {
+            options["delay"] = item[2];
+        }
+    }
 
     await captureWebsite.file(
         item[1],
